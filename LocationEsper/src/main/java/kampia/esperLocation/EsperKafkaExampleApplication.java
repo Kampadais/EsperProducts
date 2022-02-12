56c75cf933bda94242997b304587d67f289a7f43
Kampadais <csd3954@csd.uoc.gr>
2022-02-12 16:02:46 +0200

    added jackson dependancies

--- LocationEsper/src/main/java/kampia/esperLocation/EsperKafkaExampleApplication.java
+++ LocationEsper/src/main/java/kampia/esperLocation/EsperKafkaExampleApplication.java
@@ -3,7 +3,6 @@ package kampia.esperLocation;
 public class EsperKafkaExampleApplication {
 
 	public static void main(String[] args) throws Exception {
-	//	SpringApplication.run(EsperKafkaExampleApplication.class, args);
 		new EsperMain().run();
 	}
 
